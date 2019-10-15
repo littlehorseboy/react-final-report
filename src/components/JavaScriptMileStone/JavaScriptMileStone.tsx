fb7c08ad63f2c83261b623ac9caf44cc8f3b2381
horse <littlehorseboy@gmail.com>
2019-10-15 13:49:49 +0800

    feat: mileStone

--- src/components/JavaScriptMileStone/JavaScriptMileStone.tsx
+++ src/components/JavaScriptMileStone/JavaScriptMileStone.tsx
@@ -1,28 +1,54 @@
 import React, { useState, useCallback } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
 
 const useStyles = makeStyles({
+  root: {
+    marginTop: '2rem',
+  },
   pointsContainer: {
+    borderTop: '1px solid #115293',
     display: 'flex',
+  },
+  pointsWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    position: 'relative',
+  },
+  circleSvg: {
+    fill: '#115293',
+    position: 'absolute',
+  },
+  year: {
+    fontSize: '1.4rem',
+  },
+  arrowIcon: {
+    height: '2.5rem',
+    transform: 'scale(0.5, 1)',
+  },
+  content: {
+    color: '#115293',
+    fontSize: '1.1rem',
   },
 });
 
+const javascriptMileStones = [
+  { year: 1996, mileStones: ['JavaScript'] },
+  { year: 1997, mileStones: ['EcmaScript'] },
+  { year: 2004, mileStones: ['Firefox'] },
+  { year: 2006, mileStones: ['jQuery'] },
+  { year: 2008, mileStones: ['V8', 'Chrome'] },
+  { year: 2009, mileStones: ['ES5', 'Node.js'] },
+  { year: 2010, mileStones: ['npm', 'Angular'] },
+  { year: 2013, mileStones: ['React'] },
+  { year: 2014, mileStones: ['Vue.js'] },
+  { year: 2015, mileStones: ['ES6', 'Edge'] },
+];
+
 export default function JavaScriptMileStone(): JSX.Element {
   const classes = useStyles();
-
-  const javascriptMileStones = [
-    { year: 1996, content: 'JavaScript' },
-    { year: 1997, content: 'EcmaScript' },
-    { year: 2006, content: 'jQuery' },
-    { year: 2008, content: 'Chrome' },
-    { year: 2009, content: 'Node.js' },
-    { year: 2010, content: 'Angular' },
-    { year: 2013, content: 'React' },
-    { year: 2014, content: 'Vue.js' },
-    { year: 2015, content: 'ES6' },
-  ];
-
-  const pointCount = javascriptMileStones.length;
 
   const [width, setWidth] = useState(0);
 
@@ -33,14 +59,43 @@ export default function JavaScriptMileStone(): JSX.Element {
   }, []);
 
   return (
-    <div>
-      <div ref={measuredRef} style={{ borderBottom: '1px solid' }} />
-      {/* <div>{width}</div> */}
-      <div className={classes.pointsContainer}>
+    <div className={classes.root}>
+      <div ref={measuredRef} className={classes.pointsContainer}>
         {width && javascriptMileStones.map((mileStone) => (
-          <span key={mileStone.year} style={{ width: width / pointCount }}>
-            {mileStone.content}
-          </span>
+          <div
+            key={mileStone.year}
+            className={classes.pointsWrapper}
+            style={{ width: width / javascriptMileStones.length }}
+          >
+            {((): JSX.Element => {
+              const radius = 6;
+              return (
+                <svg
+                  className={classes.circleSvg}
+                  width={radius * 2}
+                  height={radius * 2}
+                  style={{ top: -radius }}
+                >
+                  <circle cx={radius} cy={radius} r={radius} />
+                </svg>
+              );
+            })()}
+
+            <br />
+
+            <div className={classes.year}>
+              {mileStone.year}
+            </div>
+
+            {mileStone.mileStones.map((content): JSX.Element => (
+              <>
+                <ArrowUpwardIcon className={classes.arrowIcon} />
+                <Typography className={classes.content}>
+                  {content}
+                </Typography>
+              </>
+            ))}
+          </div>
         ))}
       </div>
     </div>
