d5410f7290cc78bca238028a3b25a8e146008072
horse <littlehorseboy@gmail.com>
2019-10-16 14:45:56 +0800

    build: js nodeJs

--- src/components/JavaScriptMileStone/JavaScriptMileStone.stories.tsx
+++ src/components/JavaScriptMileStone/JavaScriptMileStone.stories.tsx
@@ -10,7 +10,7 @@ export interface JavascriptMileStone {
   mileStones: string[];
 }
 
-const javascriptMileStones: JavascriptMileStone[] = [
+const allMileStones: JavascriptMileStone[] = [
   { year: 1996, mileStones: ['JavaScript'] },
   { year: 1997, mileStones: ['ECMAScript'] },
   { year: 2004, mileStones: ['Firefox'] },
@@ -23,10 +23,45 @@ const javascriptMileStones: JavascriptMileStone[] = [
   { year: 2015, mileStones: ['ES6', 'Edge'] },
 ];
 
+const javascriptMileStones: JavascriptMileStone[] = [
+  { year: 1996, mileStones: ['JavaScript'] },
+  { year: 1999, mileStones: ['ES3'] },
+  { year: 2009, mileStones: ['ES5'] },
+  { year: 2015, mileStones: ['ES6'] },
+  { year: 2016, mileStones: ['ES7'] },
+  { year: 2017, mileStones: ['ES8'] },
+  { year: 2018, mileStones: ['ES9'] },
+  { year: 2019, mileStones: ['ES10'] },
+];
+
+const nodeJsMileStones: JavascriptMileStone[] = [
+  { year: 2009, mileStones: ['Node.js'] },
+  { year: 2010, mileStones: ['npm', 'Angular'] },
+  { year: 2012, mileStones: ['webpack'] },
+  { year: 2013, mileStones: ['React'] },
+  { year: 2014, mileStones: ['Vue.js'] },
+];
+
 storiesOf('JavaScriptMileStone', module)
   .addDecorator(withKnobs)
-  .add('JavaScriptMileStone', () => {
+  .add('allMileStone', () => {
+    const withKnobsMileStones = allMileStones
+      .map((mileStone) => object(mileStone.year.toString(), mileStone));
+
+    return (
+      <JavaScriptMileStone javascriptMileStones={withKnobsMileStones} />
+    );
+  })
+  .add('javaScriptMileStone', () => {
     const withKnobsMileStones = javascriptMileStones
+      .map((mileStone) => object(mileStone.year.toString(), mileStone));
+
+    return (
+      <JavaScriptMileStone javascriptMileStones={withKnobsMileStones} />
+    );
+  })
+  .add('nodeJsMileStone', () => {
+    const withKnobsMileStones = nodeJsMileStones
       .map((mileStone) => object(mileStone.year.toString(), mileStone));
 
     return (
